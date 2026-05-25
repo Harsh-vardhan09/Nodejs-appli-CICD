99ce45abdeb9831390424f1a2097709373866aa6
Aarsh-hv <harsh.vardhanp0901@gmail.com>
2026-05-26 01:47:11 +0530

    added test ci

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import express from 'express'
-const app=express();
+import express from 'express';
+const app = express();
 
-const port=process.env.PORT ?? '8080'
+const port = process.env.PORT ?? '8080';
 
-app.get('/',(req,res)=>{
-    return res.json({msg:'hello from server'})
-})
+app.get('/', (req, res) => {
+  return res.json({ msg: 'hello from server' });
+});
 
-app.listen(port,()=>{
-    console.log('server is running on PORT');
-    
-})
+app.listen(port, () => {
+  console.log('server is running on PORT');
+});
